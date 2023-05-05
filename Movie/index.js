2c3387e85b31edbee696f23bdaf9a3b78fed0785
Your Full Name <your@email.tld>
2023-05-05 17:46:13 +0200

    generalize naming convention and ports

--- Movie/index.js
+++ Movie/index.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
-app.listen(8000, () => {
-    console.log("Server running on port 8000");
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
 });
 app.get("/movies", (req, res, next) => {
     let array_movies = [
